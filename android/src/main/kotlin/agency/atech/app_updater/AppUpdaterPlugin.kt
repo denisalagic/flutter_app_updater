c0e43eac4c1497257759319deeff32e212a67b98
dalagic <denisalagichr@gmail.com>
2025-04-14 17:22:39 +0200

    init

--- android/src/main/kotlin/agency/atech/app_updater/AppUpdaterPlugin.kt
+++ android/src/main/kotlin/agency/atech/app_updater/AppUpdaterPlugin.kt
@@ -5,6 +5,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import androidx.annotation.NonNull
 
 /** AppUpdaterPlugin */
 class AppUpdaterPlugin: FlutterPlugin, MethodCallHandler {
